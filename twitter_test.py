b55c32ee12d2d64d4e080a9f6b1ed6b77bcf4e61
Syihan <symuhammad@mix.wvu.edu>
2018-12-30 02:47:16 -0500

    Modified twitter_test to use string interpolation

--- twitter_test.py
+++ twitter_test.py
@@ -7,7 +7,7 @@ from utils import ConsoleTimer
 
 # Constants
 VERSION = 1.1
-API_URL = 'https://api.twitter.com/%s' % VERSION
+API_URL = f'https://api.twitter.com/{VERSION}'
 DEFAULT_USERNAME = creds.USERNAME
 DEFAULT_PASSWORD = creds.PASSWORD
 
@@ -35,9 +35,8 @@ def get_followers(username: str=DEFAULT_USERNAME, password: str=DEFAULT_PASSWORD
     Returns:
         The user's followers.
     """
-
-    # return requests.get(API_URL + '/followers/ids.json?screen_name=%s' % username)
-    return requests.get('https://api.twitter.com/1.1/followers/ids.json?screen_name=%s' % username, auth=(username, password))
+    
+    return requests.get(f'{API_URL}/followers/ids.json?screen_name={username}', auth=(username, password))
 
 def get_following(username: str=DEFAULT_USERNAME, password: str=DEFAULT_PASSWORD):
     """Returns a user's following.
@@ -50,7 +49,7 @@ def get_following(username: str=DEFAULT_USERNAME, password: str=DEFAULT_PASSWORD
         The user's following.
     """
 
-    return requests.get(API_URL + '/friends/ids.json?screen_name=%s' % username)
+    return requests.get(f'{API_URL}/friends/ids.json?screen_name={username}')
 
 def get_unfollowers(followers: list, following: list):
     """Prints a user's unfollowing.
@@ -60,8 +59,8 @@ def get_unfollowers(followers: list, following: list):
         following {list} -- The user's following
     """
 
-    print ('Followers: \n%s' % followers)
-    print ('Following: \n%s' % following)
+    print (f'Followers: \n{followers}')
+    print (f'Following: \n{following}')
 
 # Execute the main method.
 if __name__ == '__main__':
